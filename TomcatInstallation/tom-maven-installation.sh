a607ca3dc822dba46902cabf177aad889b2b71a7
madireddyanilkumar <149090345+anilmadireddy@users.noreply.github.com>
2023-11-23 05:36:27 +0530

    Update tom-maven-installation.sh

--- TomcatInstallation/tom-maven-installation.sh
+++ TomcatInstallation/tom-maven-installation.sh
@@ -2,7 +2,7 @@ echo "-----------------------Tomcat Installation Process Start------------------
 echo "-----------------------installing the updates, upgrading and default-jdk------------------------------------------"
 echo "-----------------------sudo apt update && sudo apt upgrade -y && sudo apt install default-jdk -y------------------"
 sleep 5
-sudo apt update && sudo apt upgrade -y && sudo apt install default-jdk -y
+sudo apt update && sudo apt upgrade -y && sudo apt install default-jdk -y && sudo apt install sysvbanner -y
 
 echo "-----------------------checking the java version java --version------------------------------------------"
 sleep 5
@@ -31,7 +31,7 @@ cp /opt/apache-tomcat-9.0.83/webapps/host-manager/META-INF/context.xml /opt/apac
 cp /opt/apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml /opt/apache-tomcat-9.0.83/webapps/manager/META-INF/context1.xml
 
 
-echo "-----------------------Applying changes to context.xml and omcat-users1.xml-------------------------------------------------------------"
+echo "-----------------------Applying changes to context.xml and tomcat-users1.xml-------------------------------------------------------------"
 sleep 5
 cp conf/tomcat-users.xml /opt/apache-tomcat-9.0.83/conf/tomcat-users.xml
 cp docs/context.xml /opt/apache-tomcat-9.0.83/webapps/docs/META-INF/context.xml
@@ -44,7 +44,12 @@ sleep 5
 /opt/apache-tomcat-9.0.83/bin/startup.sh
 echo "-----------------------Now you can hit tomcat with 'http://<PUBLIC_IP>:8080'------------------------------------------------------------"
 
-echo "-----------------------downloading the tomcat apache-maven-3.9.5-bin.zip------------------------------------------"
+sleep 5
+banner end of Tomcat
+banner installation
+sleep 5
+
+echo "-----------------------downloading the apache-maven-3.9.5-bin.zip------------------------------------------"
 sleep 5
 wget -P /opt https://dlcdn.apache.org/maven/maven-3/3.9.5/binaries/apache-maven-3.9.5-bin.zip
 
@@ -68,7 +73,26 @@ sleep 10
 
 echo "-----------------------Checking Maven Version---mvn --version------------------------------------------"
 sleep 5
+source /etc/profile
 mvn --version
-sudo apt install sysvbanner -y
-banner end
 
+sleep 5
+banner end of Maven
+banner Installation
+sleep 5
+
+echo "-----------------------cloning package git------------------------------------------"
+sleep 5
+git clone https://github.com/anilmadireddy/myWebapp.git
+
+echo "-----------------------Building Package Through Maven------------------------------------------"
+source /etc/profile
+cp
+sleep 5
+
+echo "-----------------------Deploying Webapp------------------------------------------"
+cp /home/ubuntu/myAWSDevopsDocs/TomcatInstallation/myWebapp/webapp/target/webapp.war /opt/apache-tomcat-9.0.83/webapps/
+
+sleep 5
+banner end of webapp
+banner Deployment
